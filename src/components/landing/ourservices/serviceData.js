d3489d07550a2f5cde1efb6739ee2de1ed190211
Kartikaye-Mishra <kartikaye1.mishra@gmail.com>
2024-08-23 14:04:19 +0530

    fixed typos

--- src/components/landing/ourservices/serviceData.js
+++ src/components/landing/ourservices/serviceData.js
@@ -3,19 +3,19 @@ import images from '../../../images/images.js'
     {
     imgURL:images.softdev,
     title:'Software Development',
-    desc:'Cysco develops quality software and offers related services software consulting, cloud migration, app integration, and more.'
+    desc:'Webmigo develops quality software and offers related services software consulting, cloud migration, app integration, and more.'
 
 },
     {
     imgURL:images.webdev,
     title:'Website Development',
-    desc:"Cysco's web developers help brands succeed in the ever-changing digital landscape by creating customer-focused, customer-centric website solutions."
+    desc:"Webmigo's web developers help brands succeed in the ever-changing digital landscape by creating customer-focused, customer-centric website solutions."
 
 },
 {
     imgURL:images.fullStack,
     title:'Full Stack Development',
-    desc:'Full-stack developers Cysco are skilled in everything from the concept and idea to the finished product.'
+    desc:'Full-stack developers at Webmigo are skilled in everything from the concept and idea to the finished product.'
 
 },
     {
@@ -34,7 +34,7 @@ import images from '../../../images/images.js'
     {
     imgURL:images.ecom,
     title:'E-Commerce Development',
-    desc:'We offer all the skills necessary for the success of your eCommerce project under one roof with Cysco.'
+    desc:'We offer all the skills necessary for the success of your eCommerce project under one roof with Webmigo.'
 
 },
 
